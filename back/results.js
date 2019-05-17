3f34a3db0a100ad944a1922301725acf1ec1a080
Benoit C <benoit.caccinolo@gmail.com>
2019-05-18 00:59:15 +0200

    scrap results working

--- back/results.js
+++ back/results.js
@@ -25,6 +25,7 @@ let google_async = (query) => {
 }
 
 (async () => {
+  console.log(q);
   var results = await google_async(q)
   const url = results[0].link
   console.log(url);
@@ -36,42 +37,14 @@ let google_async = (query) => {
 
   await page.goto(url);
 
-  await page.screenshot({path: 'screenshot.png'});
+  // Wait for the page to be loaded with the matches list content
+  const selector = "div#livescoreMatch div.match-detail div.status-score"
+  await page.waitForSelector(selector).then(() => console.log("Page loaded"));
+
+  span_selector = "div#livescoreMatch div.match-detail div.status-score > span"
+  const elems = await page.$$eval(span_selector, (spans) => spans.map(e => e.textContent));
+  console.log(elems);
 
   await browser.close();
 })();
-
-// Main function
-(async function() {
-  // *****************************************************************=
-  // Scrapping the odds
-  // *****************************************************************=
-  const browser = await puppeteer.launch({headless:true});
-  const page = await browser.newPage();
-  const viewPort = {width:800, height:3000};
-  await page.setViewport(viewPort);
-
-  // For debug reasons: console.log in the 'page.evaluate'
-  page.on('console', consoleMessageObject => function (consoleMessageObject) {
-    if (consoleMessageObject._type !== 'warning') {
-        console.log(consoleMessageObject._text)
-    }
-
-  });
-  await page.goto(url);
-
-  // Wait for the page to be loaded with the matches list content
-  const section_selector = "#app-inner > div > div:nth-child(1) > span > div > div:nth-child(2) > section.event-list";
-  const list_selector = section_selector + ' > div:nth-of-type(2) > div:first-child > div:first-child > div:first-child';
-  await page.waitForSelector(list_selector).then(() => console.log("Match list loaded"));
-
-  const matches = await page.evaluate(utils.extractMatchesDataFromPage);
-
-  await browser.close();
-
-  console.log(matches);
-
-}) // ()
-
-
 
