8242a3c79d46f3a0acf38044a1e121c62d952960
Benoit C <benoit.caccinolo@gmail.com>
2019-05-18 08:02:39 +0200

    scrap tennis

--- back/results.js
+++ back/results.js
@@ -3,14 +3,22 @@ const google = require('google');
 
 const utils = require('./utils');
 
-const player1 = "Chorale Roanne",
-      player2 = "Vichy Clermont",
-      sport = "basket",
-      source = 'matchendirect',
-      month = 'mai',
-      year = '2019'
+// Basket - matchendirect.com
+// const player1 = "Chorale Roanne",
+//       player2 = "Vichy Clermont",
+//       sport = "basket",
+//       source = 'matchendirect',
+//       month = 'mai',
+//       year = '2019'
 
-const q = `${player1} ${player2} ${sport} ${source} ${month} ${year}`
+// Tennis - sportytrader.com
+const player1 = "Stefanos Tsitsipas",
+      player2 = "Jannik Sinner",
+      sport = "tennis",
+      source = 'sportytrader',
+      extra = '05/2019'
+
+const q = `${player1} ${player2} ${sport} ${source} ${extra}`
 
 // encapsulating google call
 let google_async = (query) => {
@@ -36,14 +44,19 @@ let google_async = (query) => {
   await page.setViewport(viewPort);
 
   await page.goto(url);
+  await page.screenshot({path: 'screenshot.png'});
+
+  // **************************************************************************
+  // sportytrader.com
+  // **************************************************************************
 
   // Wait for the page to be loaded with the matches list content
-  const selector = "div#livescoreMatch div.match-detail div.status-score"
+  const selector = "#header-sporting-event-tennis"
   await page.waitForSelector(selector).then(() => console.log("Page loaded"));
 
-  span_selector = "div#livescoreMatch div.match-detail div.status-score > span"
-  const elems = await page.$$eval(span_selector, (spans) => spans.map(e => e.textContent));
-  console.log(elems);
+  score_selector = "#header-sporting-event-tennis > div.header-match-timer > div > div.score"
+  const content = await page.$eval(score_selector, (div) => div.textContent);
+  console.log(content);
 
   await browser.close();
 })();
